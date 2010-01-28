7f975da01c13e0cc5323f133dc74af54c9e67ddb
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2010-01-28 13:47:14 +0000

    include multiwords and syllable-split words; remove syllable splitting character with relabel

--- dev/update-lexc.sh
+++ dev/update-lexc.sh
@@ -31,10 +31,10 @@ function grepextract {
     local LEXC=$2;
     local TMP=$3;
     # global $OUTFILE
-    cat $LEXC | sed 's/0//g' > $TMP;
+    cat $LEXC | sed 's/[0^#]//g' > $TMP;
     for lema in `cat $EXP | sed 's/ /_/g' | grep "$GREP" | cut -f1 -d'<' | sort | uniq`; do
 	lineno=`grep -nH -e "^ \?$lema+" -e "^ \?$lema:" -e "^ \?$lema " $TMP | cut -f2 -d':'`;
-	# TODO: for i in $lineno, since we may have several entries per lema
+	# for i in $lineno, since we may have several entries per lema
 	if [ "$lineno" != "" ]; then
             for ret in $lineno; do	
 	    	head -n$ret $LEXC | tail -1 >> $OUTFILE;
