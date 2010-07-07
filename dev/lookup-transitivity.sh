2436636417d9352163415db5eeafdea3a8c03115
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2010-07-07 11:58:07 +0000

    added transitivity tags using GTSVN/sme.hfst (some verbs were not there though, marked TODO instead of TV/IV)

---
+++ dev/lookup-transitivity.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+### Usage: cat dev/inc-sme-nob-dic-verb.txt | dev/lookup-transitivity.sh
+### Note: If the <l> (sme lemma) has <b/> or other tags in it, this will fail
+
+if [ ! -e $GTHOME  ]; then
+	echo "Please set the \$GTHOME environment variable";
+	exit;
+fi
+FST=$GTHOME/trunk/gt/sme/bin/sme.hfst.ol
+
+sed 's/.*<l>//'| sed 's%</l>\(.*\)$%[\1]%' |\
+hfst-proc $FST |\
+while read line; do
+    lemma=$(echo $line | sed 's%\^\([^/]*\).*%\1%')
+    rhs=$(echo $line | gsed 's%.*\[\(.*\)\].*%\1%')
+    valences=$(echo $line | sed 's%\[.*%%' | tr '/' '\n' | grep "^${lemma}+V+" | grep -v "+Der" | sed 's/.*\+\([TI]V\)\+.*/\1/' | uniq)
+    if [ ${#valences} -eq 0 ]; then
+	echo "    <e><p><l>${lemma}<s n=\"V\"/><s n=\"TODO\"/></l>${rhs}"
+    fi;
+    for trans in ${valences}; do
+	echo "    <e><p><l>${lemma}<s n=\"V\"/><s n=\"${trans}\"/></l>${rhs}"
+    done
+done
