ad0d4d3dcef7c9186c5236f0064e2a31f1299b32
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2011-11-03 12:14:14 +0000

    new generation-report

--- dev/generation-test.sh
+++ dev/generation-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DEV=`dirname $0`
+TMP=/tmp
 
 if [[ $# -lt 1 ]]; then
 	echo "Not enough arguments to generation-test.sh -r";
@@ -17,53 +18,53 @@ if [[ $1 == "-r" ]]; then
 	args=("$@")
 	echo "Corpus in: "`dirname $2`;
 	echo -n "Processing corpus for generation test... ";
-	rm -f /tmp/sme-nob.corpus.txt
+	rm -f $TMP/sme-nob.corpus.txt
 	for ((i=1; i <= $# ; i++)); do 
 		if [[ ${args[$i]} != "" && -f ${args[$i]} ]]; then 
-			cat ${args[$i]} >> /tmp/sme-nob.corpus.txt
+			cat ${args[$i]} >> $TMP/sme-nob.corpus.txt
 		fi
 	done
 	echo "done.";
 	echo -n "Translating corpus for generation test (this could take some time)... ";
-	cat /tmp/sme-nob.corpus.txt | apertium -d ${DEV}/../ sme-nob-postchunk | sed 's/\$\W*\^/$\n^/g' > /tmp/sme.gentest.postchunk
+	cat $TMP/sme-nob.corpus.txt | apertium -d ${DEV}/../ sme-nob-postchunk | sed 's/\$\W*\^/$\n^/g' > $TMP/sme.gentest.postchunk
 	echo "done.";
 fi
 
-if [[ ! -f /tmp/sme.gentest.postchunk ]]; then
+if [[ ! -f $TMP/sme.gentest.postchunk ]]; then
 	echo "Something went wrong in processing the corpus, you have no output file.";
 	echo "Try running:"
 	echo "   sh generation-test.sh -r <corpus>";
 	exit;
 fi
 
-sed 's/^ //g' < /tmp/sme.gentest.postchunk | grep -v -e '@' -e '*' -e '[0-9]<Num>' -e '#}' -e '#{' | sed 's/\$>/$/g' | LC_ALL='C' sort -f | uniq -c | sort -gr > /tmp/sme.gentest.stripped
-lt-proc -d ${DEV}/../sme-nob.autogen.bin < /tmp/sme.gentest.stripped > /tmp/sme.gentest.surface
-sed 's/^ *[0-9]* \^/^/g'                 < /tmp/sme.gentest.stripped > /tmp/sme.gentest.nofreq
-paste /tmp/sme.gentest.surface /tmp/sme.gentest.nofreq | grep -e '\/' -e '^[^	]*#'  > /tmp/sme.generation.errors.txt
-grep -v '^[^	]*#' /tmp/sme.generation.errors.txt | grep    '\/' > /tmp/sme-nob.multiform
-grep    '^[^	]*#' /tmp/sme.generation.errors.txt | grep    '\/' > /tmp/sme-nob.multibidix
-grep    '^[^	]*#' /tmp/sme.generation.errors.txt | grep -v '\/' > /tmp/sme-nob.tagmismatch 
+sed 's/^ //g' < $TMP/sme.gentest.postchunk | grep -v -e '@' -e '*' -e '[0-9]<Num>' -e '#}' -e '#{' | sed 's/\$>/$/g' | LC_ALL='C' sort -f | uniq -c | sort -gr > $TMP/sme.gentest.stripped
+lt-proc -d ${DEV}/../sme-nob.autogen.bin < $TMP/sme.gentest.stripped > $TMP/sme.gentest.surface
+sed 's/^ *[0-9]* \^/^/g'                 < $TMP/sme.gentest.stripped > $TMP/sme.gentest.nofreq
+paste $TMP/sme.gentest.surface $TMP/sme.gentest.nofreq | grep -e '\/' -e '^[^	]*#'  > $TMP/sme.generation.errors.txt
+grep -v '^[^	]*#' $TMP/sme.generation.errors.txt | grep    '\/' > $TMP/sme-nob.multiform
+grep    '^[^	]*#' $TMP/sme.generation.errors.txt | grep    '\/' > $TMP/sme-nob.multibidix
+grep    '^[^	]*#' $TMP/sme.generation.errors.txt | grep -v '\/' > $TMP/sme-nob.tagmismatch 
 
 echo "";
 echo "===============================================================================";
 echo "Multiple surface forms for a single lexical form";
 echo "===============================================================================";
-cat /tmp/sme-nob.multiform
+cat $TMP/sme-nob.multiform
 
 echo "";
 echo "===============================================================================";
 echo "Multiple bidix entries for a single source language lexical form";
 echo "===============================================================================";
-cat /tmp/sme-nob.multibidix
+cat $TMP/sme-nob.multibidix
 
 echo "";
 echo "===============================================================================";
 echo "Tag mismatch between transfer and generation";
 echo "===============================================================================";
-cat /tmp/sme-nob.tagmismatch
+cat $TMP/sme-nob.tagmismatch
 
 echo "";
 echo "===============================================================================";
 echo "Summary";
 echo "===============================================================================";
-wc -l /tmp/sme-nob.multiform /tmp/sme-nob.multibidix /tmp/sme-nob.tagmismatch
+wc -l $TMP/sme-nob.multiform $TMP/sme-nob.multibidix $TMP/sme-nob.tagmismatch
