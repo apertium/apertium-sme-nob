d216465c711f54b7bf3886a4934c0da10a56bfb2
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2011-11-09 11:13:17 +0000

    new report, also multiple generation-grep hits with -m option

--- dev/generation-grep.sh
+++ dev/generation-grep.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+maxhits=1
+OPTIND=1; while getopts "m:" opt; do
+  case "$opt" in
+    m) maxhits=$OPTARG;;
+  esac
+done
+shift $((OPTIND-1))
+
 if [ $# -ne 2 ];then
     echo "Usage:"
-    echo "\$ ./$0 'word<tag><tag>' corpus.pilcrowed"
+    echo "\$ ./$0 [-m2] 'word<tag><tag>' corpus.pilcrowed"
     echo "where 'word<tag><tag> is from the dev/generation-test.sh report"
+    echo "and -mN means return a max of N hits (default:1)"
     echo ""
     echo "Assumes corpus was preprocessed like:"
     echo "\$ awk 'p&&/^ *$/{print \"|\"NR\"¶\";p=0} {print} /[^ ]/{p=1}' corpus > corpus.pilcrowed"
@@ -22,19 +31,22 @@ TMP=/tmp
 POSTCHUNKOUT="$TMP/sme.gentest.postchunk"
 
 extract () {
-    awk -F"\\\^|<det><qnt>" "
-/${1}/ && n && seen==0 { print n; seen=1 }
+    awk -vm="$maxhits" -F"\\\^|<det><qnt>" "
+/${1}/ && n && seen==0 { printf(\"%s:\",n); seen=1 }
 /¶/                    { n=prev }
-/¶/ && seen            { print n; exit } 
+/¶/ && seen            { print n; seen=0; if(--m==0) exit;} 
 /<det><qnt>/           { prev=\$2 }
 " "$2";
 }
 
-IFS=$' \t\n' read -a lines <<< `extract "${needle}" "$POSTCHUNKOUT"`
-if [ "${#lines[@]}" == "2" ]; then
-    echo "found ${needle} between lines ${lines[0]} and ${lines[1]}" 1>&2
-    awk "/^\|${lines[0]}$pilcrow\$/,/^\|${lines[1]}$pilcrow$/{print}" "$CORPUS"
-else
-    echo "Couldn't find ${needle}!" 1>&2
-    exit 1
-fi
+foundone=false
+IFS=$' \t\n' read -a allhits <<< `extract "${needle}" "$POSTCHUNKOUT"`
+for h in "${allhits[@]}"; do
+    IFS=$':' read -a hit <<< "$h"
+    if [ "${#hit[@]}" == "2" ]; then
+	echo "found ${needle} between lines ${hit[0]} and ${hit[1]}" 1>&2
+	awk "/^\|${hit[0]}$pilcrow\$/,/^\|${hit[1]}$pilcrow$/{print}" "$CORPUS"
+	foundone=true
+    fi
+done
+$foundone || ( echo "Couldn't find ${needle}!" 1>&2; exit 1; )
