c39db50da4924fe1f933623731ad390f400a1db1
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2013-11-13 14:15:42 +0000

    minor meh

--- pending-tests.sh
+++ pending-tests.sh
@@ -2,12 +2,9 @@
 
 C=2
 GREP='.'
-if [ $# -eq 1 ]
-then
+if [[ $# -eq 1 ]]; then
     C=$1
     GREP='WORKS'
 fi
 
-sh wiki-tests.sh Pending sme nob update | grep -C $C "$GREP"
-
-
+./wiki-tests.sh Pending sme nob update | grep -C $C "$GREP"
--- regression-tests.sh
+++ regression-tests.sh
@@ -2,10 +2,9 @@
 
 C=2
 GREP='.'
-if [ $# -eq 1 ]; then
+if [[ $# -eq 1 ]]; then
     C=$1
     GREP='^	-'
 fi
 
-sh wiki-tests.sh Regression sme nob update | grep -C $C "$GREP"
-
+./wiki-tests.sh Regression sme nob update | grep -C $C "$GREP"
