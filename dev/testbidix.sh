3f9a1f9322465f719b3858ac3c9c8626d34dc0b4
Lene Antonsen <lene.antonsen@uit.no>
2014-04-20 20:54:47 +0000

    Correction

--- dev/testbidix.sh
+++ dev/testbidix.sh
@@ -1,7 +1,6 @@
 #Find words in bidix which are not in the fst or the PoS or other tags are not correct
 
 echo "MISSING words in bidix" > $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/testgenreport.txt
-grep '<l>' $HOME/apertium/trunk/apertium-sme-nob/apertium-sme-nob.sme-nob.dix | egrep -v '(<l><|^\!|^<\!)' | grep -v '<par .*<p>' | sed 's/<b\/>/ /' | tr "<" ">" | cut -d ">" -f7 | $HLOOKUP $HOME/apertium/trunk/apertium-sme-nob/sme-nob.automorf.hfst | grep '+?' |cut -f1 >> $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/missing.txt
-
+grep '<l>' $HOME/apertium/trunk/apertium-sme-nob/apertium-sme-nob.sme-nob.dix | egrep -v '(<l><|^\!|^<\!|prop)' | grep -v '<par .*<p>' | grep '"n"' | sed 's/<b\/>/ /' | tr "<" ">" | cut -d ">" -f7 | $HLOOKUP $HOME/apertium/trunk/apertium-sme-nob/sme-nob.automorf.hfst | grep '+?' |cut -f1 | sort >> $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/testgenreport.txt
 
 
