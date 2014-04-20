fa92a07de0f51928085416c8500e53016e8a3d1b
Lene Antonsen <lene.antonsen@uit.no>
2014-04-20 20:24:27 +0000

    Find words in bidix which are not in the fst or the PoS or other tags are not correct.

---
+++ dev/testbidix.sh
@@ -0,0 +1,7 @@
+#Find words in bidix which are not in the fst or the PoS or other tags are not correct
+
+echo "MISSING words in bidix" > $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/testgenreport.txt
+grep '<l>' $HOME/apertium/trunk/apertium-sme-nob/apertium-sme-nob.sme-nob.dix | egrep -v '(<l><|^\!|^<\!)' | grep -v '<par .*<p>' | sed 's/<b\/>/ /' | tr "<" ">" | cut -d ">" -f7 | $HLOOKUP $HOME/apertium/trunk/apertium-sme-nob/sme-nob.automorf.hfst | grep '+?' |cut -f1 >> $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/missing.txt
+
+
+
