11bb4101b6f3feacb95a90b7d64a6fc11eea59a0
Lene Antonsen <lene.antonsen@uit.no>
2014-04-25 13:45:49 +0000

    Corrections

--- dev/testbidix.sh
+++ dev/testbidix.sh
@@ -3,4 +3,5 @@
 echo "MISSING words in bidix" > $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/testgenreport.txt
 grep '<l>' $HOME/apertium/trunk/apertium-sme-nob/apertium-sme-nob.sme-nob.dix | egrep -v '(<l><|^\!|^<\!)' | grep -v '<par .*<p>' | sed 's/<b\/>/ /' | tr "<" ">" | cut -d ">" -f7 | $HLOOKUP $HOME/apertium/trunk/apertium-sme-nob/sme-nob.automorf.hfst | grep '+?' |cut -f1 | sort >> $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/testgenreport.txt
 
+see $HOME/apertium/trunk/apertium-sme-nob/dev/testgen/testgenreport.txt
 
