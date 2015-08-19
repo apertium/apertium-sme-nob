f7a98d0cca3ed7fad61a770c1c5287b6d541a222
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-08-19 21:17:56 +0000

    debug script

--- dev/compare-our-cg-with-gt.sh
+++ dev/compare-our-cg-with-gt.sh
@@ -3,10 +3,11 @@
 D="$(dirname $0)/.."
 
 input=$(mktemp -t XXXXXXXXXXX.smenob)
+trap 'rm -f "${input}"' EXIT
 cat >"${input}"
 
 ours () {
-<"${input}" hfst-proc -w -p "$D"/sme-nob.automorf-untrimmed.hfst |cg-conv -a|vislcg3 --grammar "$D"/sme-nob.mor.rlx.bin
+    <"${input}" hfst-proc -w -p "$D"/sme-nob.automorf-untrimmed.hfst |cg-conv -a|vislcg3 --grammar "$D"/sme-nob.mor.rlx.bin
 }
 
 xerox-to-cg () {
@@ -14,13 +15,11 @@ xerox-to-cg () {
 }
 
 gt () {
-    # Note: hfst-proc -C does weird stuff to compounds; e.g. we want subreadings, not the # stuff
-    # but cg-conv -a also fails due to /-es
-<"${input}" hfst-proc -x "${GTHOME}"/langs/sme/tools/mt/apertium/analyser-mt-gt-desc.nob.hfstol|xerox-to-cg|vislcg3 -g "${GTHOME}"/langs/sme/tools/mt/apertium/disambiguation-mt-gt.cg3
+    <"${input}" hfst-proc -x "${GTHOME}"/langs/sme/tools/mt/apertium/analyser-mt-gt-desc.nob.hfstol|xerox-to-cg|vislcg3 -g "${GTHOME}"/langs/sme/tools/mt/apertium/disambiguation-mt-gt.cg3
 
 }
 
-diff -C3 -w \
+diff -U10 -w \
      <(gt | tr '[:upper:]' '[:lower:]'|tr '/' _ |sed 's/<sme>//g' | tr -s ' ')  \
      <(ours | tr '[:upper:]' '[:lower:]'|tr '/' _ | sed 's/←/</g;s/→/>/g;s/vblex/v/g;s/pre\([st]\)/pr\1/g;s/adj/a/g;s/indic/ind/g;s/cnjcoo/cc/;s/prn/pron/g')
 
