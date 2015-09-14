be0a26396c491c2b71398fd0e654b783eea8ee57
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-09-14 10:54:05 +0000

    tagfixes

--- dev/sme-nob.inconsistency.sh
+++ dev/sme-nob.inconsistency.sh
@@ -75,11 +75,11 @@ adj () {
     rhsbidix |\
 grep '<s n="adj"/>' |\
 grep -v '<s n="adj"/><s n="pprs"/>' |\
-sed 's%<s n="adj"/><s n="sint"/>.*<par n="pl__adj"/>%<adj><sint><posi><pl>$%' |\
-sed 's%<s n="adj"/>.*<par n="pl__adj"/>%<adj><posi><pl>$%' |\
-sed 's%<s n="adj"/><s n="ord"/>.*%<adj><ord><posi><mf><sg><ind>$%' |\
-sed 's%<s n="adj"/><s n="sint"/>.*%<adj><sint><posi><mf><sg><ind>$%' |\
-sed 's%<s n="adj"/>.*%<adj><posi><mf><sg><ind>$%'
+sed 's%<s n="adj"/><s n="sint"/>.*<par n="pl__adj"/>%<adj><sint><pst><pl>$%' |\
+sed 's%<s n="adj"/>.*<par n="pl__adj"/>%<adj><pst><pl>$%' |\
+sed 's%<s n="adj"/><s n="ord"/>.*%<adj><ord><pst><mf><sg><ind>$%' |\
+sed 's%<s n="adj"/><s n="sint"/>.*%<adj><sint><pst><mf><sg><ind>$%' |\
+sed 's%<s n="adj"/>.*%<adj><pst><mf><sg><ind>$%'
 }
 
 pr () {
