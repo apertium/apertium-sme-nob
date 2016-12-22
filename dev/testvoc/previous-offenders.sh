7295554bbad9ee10cc487008f81a806aac1256ae
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-12-22 09:52:17 +0000

    dev/testvoc/generation.sh --hfst sme-nob
    works to find #-es over the full analyser (excluding FST-loops), but
    is quite slow, and probably has some false positives (e.g. CG removes
    non-cog/top/ant-marked np's)
    
    dev/testvoc/previous-offenders.sh has stuff from Regression-tests that
    was mainly there to test generation errors (we can now make Regression_tests
    more about translation quality and less about #)
    
    bidix-unknowns.sh and test-ngrams.sh are works in progress

---
+++ dev/testvoc/previous-offenders.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e -u
+
+tests=(
+    "1 () 1998-99 Utjamningsmeldinga sátni."
+    "1 () 1998-99 Utjamningsmeldinga sátni"
+    "1300-."
+    "1814 Vuođđolága."
+    "(1998-99) Utjamningsmeldinga."
+    "50.0 %."
+    "50 %."
+    "Anne Tornensis Hætta."
+    "buohtastahttit eará gielaiguin."
+    "čakčamánu 7.b. 1854"
+    "Dalle ledjen juo barggus Romssa Sámeskuvllas."
+    "Doai váldibeahtti dušše iežade biergasiid mielde."
+    "eanan-"
+    "eanan-."
+    "eanan- ja luonddudoahpagiid"
+    "eanan- ja luonddudoahpagiid."
+    "El."
+    "Fertebeahtti jáhkkigoahtit dakkár balddonasmuitalusaide."
+    "ieš dat muitalii."
+    "ieš mun dájun."
+    "ieža moai dáppe váccašetne."
+    "Ja de álggii muohttit ja šattai nu čoaskkis ahte galbmoje su biktasat."
+    "juohke lágán geđggiiguin."
+    "Jus in livčče mun ieš."
+    "Karen Ellen Marie Siri Utsi čájeha gokko beana lei vuddjon."
+    "Leago dát duođasge Sáme Radio oaivil?"
+    "Leimmet muhtun oahppásat hálešteamen gaskaneamet oasi sámegillii oasi dárogillii."
+    "Lei unnán mii earuhii nuorra Prinssa skuvlavázzima eará skuvlamánáin."
+    "Máhtte ii dáidde jienastit Bargiidbellodaga doppe."
+    "Máhttu lea ieš namma."
+    "Mii háliidit diehtit dáhpáhuvvá go mihkkege, ja jos nu dagaš, makkár áigemeriid siste dán áššis."
+    "min nuoramus olbmot."
+    "Mis leat alddámet ain šibihat."
+    "Moai válde dušše iežame biergasiid mielde."
+    "Mot leaččai dasto mánáide dain leairrain?"
+    "Mu bealis dat dušše nannejuvvui."
+    "Mun ceggen vaikke ieš filbmafestivála."
+    "munno."
+    "NRK Radio oaivil."
+    "ovddasvástideigga soai."
+    "Ruvdnaprinseassa Märtha ja moai letne."
+    "Sámi allaskuvla lea okta dain allaskuvllain."
+    "Sámi allaskuvla lea okta dain allaskuvllain mii čavčča rájes doaibmá."
+    "Smávva muorrakássat ledje beankan ja dasa vel lássin min iežamet niestebumbbát."
+    "u-vearba Burton-upon-Trent"
+    "Váldde biergasiid iežat fárrui."
+)
+
+
+declare -a TMPFILES
+cleanup () {
+    for f in "${TMPFILES[@]}"; do
+        rm -f "$f"
+    done
+}
+trap 'cleanup' EXIT
+
+
+inp=$(mktemp -t knownhashtestvoc.XXXXXXXXXXX)
+TMPFILES+=("${inp}")
+res=$(mktemp -t knownhashtestvoc.XXXXXXXXXXX)
+TMPFILES+=("${res}")
+
+cd "$(dirname "$0")/../.."
+printf "%s\n" "${tests[@]}" >"${inp}"
+apertium -d . sme-nob < "${inp}" >"${res}"
+bad=$(grep -c '#' "${res}")
+if [[ ${bad} -gt 0 ]]; then
+    echo "=== Still giving generation errors: ==="
+    paste -d '\n' "${inp}" "${res}" /dev/null | grep -B1 '#'
+    echo "=== Errors: ${bad}/${#tests[@]} ==="
+else
+    echo '=== All good! ==='
+fi
